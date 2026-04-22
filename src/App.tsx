9227bf9c112847200daf882e40f8cb7b3bf946bf
Maksymilian Gala <maksymilian.gala1@gmail.com>
2026-04-22 14:13:27 +0200

    fix: base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CompetitionPage from "./pages/CompetitionPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/competition/:competitionId" element={<CompetitionPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
